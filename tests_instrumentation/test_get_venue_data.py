c12288c906ac80c212c8ca4941223a9bade2c177
blister-pack <andrecrujo@hotmail.com>
2025-01-26 20:17:36 +0100

    moved test_get_venue_data and finished tests

--- tests_instrumentation/test_get_venue_data.py
+++ tests_instrumentation/test_get_venue_data.py
@@ -36,8 +36,36 @@ from fastapi import HTTPException
                 ],
             },
         ),
-        ("home-assignment-venue-berlin", None),
-        ("home-assignment-venue-tokyo", None),
+        (
+            "home-assignment-venue-berlin",
+            {
+                "venue_coordinates": [13.4536149, 52.5003197],
+                "order_minimum_no_surcharge": 1000,
+                "base_price_for_delivery": 190,
+                "distance_ranges_for_delivery": [
+                    {"min": 0, "max": 500, "a": 0, "b": 0, "flag": None},
+                    {"min": 500, "max": 1000, "a": 100, "b": 0, "flag": None},
+                    {"min": 1000, "max": 1500, "a": 200, "b": 0, "flag": None},
+                    {"min": 1500, "max": 2000, "a": 200, "b": 1, "flag": None},
+                    {"min": 2000, "max": 0, "a": 0, "b": 0, "flag": None},
+                ],
+            },
+        ),
+        (
+            "home-assignment-venue-tokyo",
+            {
+                "venue_coordinates": [139.7115264, 35.6459122],
+                "order_minimum_no_surcharge": 700,
+                "base_price_for_delivery": 49,
+                "distance_ranges_for_delivery": [
+                    {"min": 0, "max": 500, "a": 0, "b": 0, "flag": None},
+                    {"min": 500, "max": 1000, "a": 30, "b": 0, "flag": None},
+                    {"min": 1000, "max": 1500, "a": 50, "b": 0, "flag": None},
+                    {"min": 1500, "max": 2000, "a": 50, "b": 1, "flag": None},
+                    {"min": 2000, "max": 0, "a": 0, "b": 0, "flag": None},
+                ],
+            },
+        ),
     ],
 )
 def test_multiple_venue_slugs(venue_slug, expected_data_dict):
