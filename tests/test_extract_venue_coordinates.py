afb77048e2c0dd0584b68343a3023ba4aed70e73
blister-pack <andrecrujo@hotmail.com>
2025-01-26 14:00:22 +0100

    extract_venue_coordinates tests complete

--- tests/test_extract_venue_coordinates.py
+++ tests/test_extract_venue_coordinates.py
@@ -1,13 +1,24 @@
+from fastapi import HTTPException
 from source.dopc import extract_venue_coordinates
 import pytest
 
 
 @pytest.mark.parametrize(
-    "coordinate_list, expected_return",
+    "coordinate_list, expected_return, should_raise_exception",
     [
-        ([12.123532, 56.156323], (12.123532, 56.156323)),
-        ([12.123456, 45.123456, 78.123456])
+        ([12.123532, 56.156323], (12.123532, 56.156323), False),
+        ([12.123456, 45.123456, 78.123456], None, True),
+        ([12.123456], None, True),
+        ([], None, True),
     ],
 )
-def test_multiple_coordinate_lists():
-    pass
+def test_multiple_coordinate_lists(
+    coordinate_list, expected_return, should_raise_exception
+):
+    if should_raise_exception:
+        with pytest.raises(HTTPException) as exception_info:
+            extract_venue_coordinates(coordinate_list)
+        assert exception_info.value.status_code == 400
+        assert exception_info.value.detail == "Can only take in 2 arguments for coordinates -> [latitude, longitude]"  # fmt:skip
+    else:
+        assert extract_venue_coordinates(coordinate_list) == expected_return
