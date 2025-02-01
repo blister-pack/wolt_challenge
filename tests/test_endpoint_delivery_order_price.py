630a6e8ee3c40419211f88251701b1dc5c0cac99
blister-pack <andrecrujo@hotmail.com>
2025-02-01 23:54:12 +0100

    reqs change because testing endpoint adds starlette and httpx

--- tests/test_endpoint_delivery_order_price.py
+++ tests/test_endpoint_delivery_order_price.py
@@ -4,6 +4,20 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 
-def test_delivery_order_price():
+def test_dopc_no_args():
     response = client.get("/api/v1/delivery-order-price")
+    assert response.status_code == 422
+
+
+def test_dopc_bad_args():
+    response = client.get(
+        "/api/v1/delivery-order-price?venue_slug=nope&cart_value=0&user_lat=1&user_lon=1"
+    )
+    assert response.status_code == 400
+
+
+def test_dopc_good_args():
+    response = client.get(
+        "/api/v1/delivery-order-price?venue_slug=home-assignment-venue-helsinki&cart_value=199&user_lat=24.93913512&user_lon=60.18112143"
+    )
     assert response.status_code == 200
