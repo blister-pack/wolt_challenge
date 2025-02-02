f715327fa2e198fcef9cc0499969f802da7b88d0
blister-pack <andrecrujo@hotmail.com>
2025-02-02 19:51:37 +0100

    test get_venue_data success

--- tests/test_get_venue_data.py
+++ tests/test_get_venue_data.py
@@ -8,18 +8,61 @@ this is similar to the tests in instrumentation, but in this case we seek out to
 test the functionality of the function without having it depend on an external API.
 """
 
+static_success_response = {"venue_raw": {"location": {"coordinates": [1, 2]}}}
 
+dynamic_success_response = {
+    "venue_raw": {
+        "delivery_specs": {
+            "order_minimum_no_surcharge": 1000,
+            "delivery_pricing": {
+                "base_price": 190,
+                "distance_ranges": [
+                    {"min": 0, "max": 500, "a": 0, "b": 0, "flag": None},
+                    {"min": 500, "max": 1000, "a": 100, "b": 0, "flag": None},
+                    {"min": 1000, "max": 1500, "a": 200, "b": 0, "flag": None},
+                    {"min": 1500, "max": 2000, "a": 200, "b": 1, "flag": None},
+                    {"min": 2000, "max": 0, "a": 0, "b": 0, "flag": None},
+                ],
+            },
+        }
+    }
+}
+
+
+@mock.patch("requests.get")
 def test_gvd_static_fail():
     pass
 
 
+@mock.patch("requests.get")
 def test_gvd_dynamic_fail():
     pass
 
 
+@mock.patch("requests.get")
 def test_gvd_static_and_dynamic_fail():
     pass
 
 
-def test_gvd_success():
+@mock.patch("requests.get")
+def test_gvd_KeyError():
     pass
+
+
+@mock.patch("requests.get")
+def test_gvd_success(mock_get):
+    mock_static = mock.Mock()
+    mock_static.status_code = 200
+    mock_static.json.return_value = static_success_response
+
+    mock_dynamic = mock.Mock()
+    mock_dynamic.status_code = 200
+    mock_dynamic.json.return_value = dynamic_success_response
+
+    mock_get.side_effect = [mock_static, mock_dynamic]
+
+    venue_data = get_venue_data("test_venue")
+
+    assert venue_data["venue_coordinates"] == [1, 2]
+    assert venue_data["base_price_for_delivery"] == 190
+    assert venue_data["order_minimum_no_surcharge"] == 1000
