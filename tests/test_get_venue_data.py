c5e6428435eb4f64e95886a3c8546c23aa2babde
blister-pack <andrecrujo@hotmail.com>
2025-02-02 16:18:42 +0100

    defined what to test from get_venue_data

--- tests/test_get_venue_data.py
+++ tests/test_get_venue_data.py
@@ -1,8 +1,25 @@
 import pytest
 from source.venue_client import get_venue_data
 from fastapi import HTTPException
+import unittest.mock as mock
 
 """
 this is similar to the tests in instrumentation, but in this case we seek out to
 test the functionality of the function without having it depend on an external API.
 """
+
+
+def test_gvd_static_fail():
+    pass
+
+
+def test_gvd_dynamic_fail():
+    pass
+
+
+def test_gvd_static_and_dynamic_fail():
+    pass
+
+
+def test_gvd_success():
+    pass
