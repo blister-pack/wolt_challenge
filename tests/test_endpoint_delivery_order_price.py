9cb6895e655d04157b68e25c6bbfd3dfa1c8ab08
blister-pack <andrecrujo@hotmail.com>
2025-02-04 00:19:34 +0100

    progress testing endpoint

--- tests/test_endpoint_delivery_order_price.py
+++ tests/test_endpoint_delivery_order_price.py
@@ -1,7 +1,52 @@
 from source.dopc import app
 from fastapi.testclient import TestClient
+import unittest.mock as mock
+from fastapi import HTTPException
+from source.venue_client import get_venue_data
 
 client = TestClient(app)
+
+mock_venue_data = {
+    "venue_coordinates": [1, 2],
+    "order_minimum_no_surcharge": 100,
+    "base_price_for_delivery": 299,
+    "distance_ranges_for_delivery": {
+        {"min": 0, "max": 1000, "price": 500},
+        {"min": 1000, "max": 2000, "price": 1000},
+    },
+}
+
+
+def mock_get_venue_data(venue_slug):
+    return mock_venue_data
+
+
+def mock_get_distance(venue_coordinates, user_coordinates):
+    return 500
+
+
+@mock.patch(
+    target="source.delivery_price_logic.get_distance",
+    side_effect=mock_get_distance,
+)
+@mock.patch(
+    target="source.venue_client.get_venue_data",
+    side_effect=mock_get_venue_data,
+)
+def test_dopc_success(mock_get_venue_data, mock_get_distance):
+    response = client.get(
+        "/api/v1/delivery-order-price",
+        params={
+            "venue_slug": "test_venue",
+            "cart_value": 1500,
+            "user_lat": 3,
+            "user_lon": 4,
+        },
+    )
+
+    assert response.status_code == 200
+    # TODO finish test with assert statements
+    # TODO solve problems with dict on line 13
 
 
 def test_dopc_no_args():
@@ -9,7 +54,7 @@ def test_dopc_no_args():
     assert response.status_code == 422
 
 
-def test_dopc_bad_args():
+def test_dopc_bad_slug():
     response = client.get(
         "/api/v1/delivery-order-price?venue_slug=nope&cart_value=0&user_lat=1&user_lon=1"
     )
